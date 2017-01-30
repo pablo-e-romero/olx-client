ddf8bbcb6397b21f32aae3925ad635edfb309d2b
Pablo Romero <romero7116@gmail.com>
2017-01-30 11:46:57 -0300

    Removed gloss.

--- olx-client/Pods/Target Support Files/Pods-olx-client/Pods-olx-client-frameworks.sh
+++ olx-client/Pods/Target Support Files/Pods-olx-client/Pods-olx-client-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Gloss/Gloss.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Gloss/Gloss.framework"
 fi
